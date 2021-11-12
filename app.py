767dabec347c0eae37e36f656ea033c177c34d23
Mittanand <Anand.M.Mittal.22@dartmouth.edu>
2021-11-12 18:47:30 -0500

    pls work

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def classify_img():
     classifier = Classifier()
     if request.method == 'POST':
         s3_url = request.json['url']
-        img_filepath = download_url(s3_url)
+        img_filepath = download_url(str(s3_url))
         classifier = Classifier()
         classification, calories = classifier.classify(img_filepath)
         if classification:
