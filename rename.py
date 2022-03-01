1edffed5171fb064f6ed1f1f2962bfe69a14a39d
Brandon Feng <brandon.y.feng.22@dartmouth.edu>
2022-02-28 22:39:21 -0500

    function for reindexing training images

--- rename.py
+++ rename.py
@@ -1,17 +1,25 @@
 import os
+import cv2
+from models.Classifier import Classifier
 
-# j = 1
-# for filename in os.listdir("train_images/Banana"):
-    # if filename == "script.sh" or filename == "script.sh~":
-    #     continue
-#     else:
-#         os.rename("train_images/Banana/" + filename,"train_images/Banana/"+"2_"+str(j)+".jpg")
-#         j+=1
+FOLDER_PATH = "train_images_unit"
 
-j = 1
-for filename in os.listdir("train_images_unit_cleaned/watermelon"):
-    # if filename == "script.sh" or filename == "script.sh~":
-    #     continue
-    # else:
-    os.rename("train_images_unit_cleaned/watermelon/" + filename, "train_images_unit_cleaned/watermelon/"+"11_"+str(j)+".jpg")
-    j += 1
+def reindex(folder_path):
+
+    classifier = Classifier()
+
+    for food_type in os.listdir(folder_path):
+        print(food_type)
+        try:
+            food_index = classifier.index2classification.index(food_type.lower())
+        except ValueError:
+            continue
+        i = 1
+        print(i)
+        for filename in os.listdir(folder_path + "/" + food_type):
+            if filename == "script.sh" or filename == "script.sh~":
+                continue
+            os.rename(folder_path + "/" + food_type + "/" + filename, folder_path + "/" + food_type + "/" + str(food_index) + "_" + str(i)+".jpg")
+            i += 1
+
+reindex(FOLDER_PATH)
